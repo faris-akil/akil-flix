fbc80daf8ce2ab63cdcb05d81e1b543502cd5846
faris-akil <akil8989@gmail.com>
2020-11-15 17:34:44 +0800

    change from http to https for omdb

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default function getMovie(type) {
     let movieApi = "b8357d1e"
     return axios.create({
-        baseURL: `http://www.omdbapi.com/?apikey=${movieApi}&${type}`
+        baseURL: `https://www.omdbapi.com/?apikey=${movieApi}&${type}`
     })
 }
